0752e32fbf2b3d860d736b89c497df14d2d5c954
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2024-07-26 23:14:52 -0500

    Create Features.js

---
+++ src/components/Features.js
@@ -0,0 +1,13 @@
+const Features = () => (
+  <section id="features">
+    <h2>Features</h2>
+    <ul>
+      <li>Over 100 miles of range</li>
+      <li>Futuristic design</li>
+      <li>Advanced battery technology</li>
+      <li>High-speed performance</li>
+    </ul>
+  </section>
+);
+
+export default Features;
