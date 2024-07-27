7b6a6c3fd2c61d7a682a75d532376473b0d853db
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2024-07-26 23:13:18 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Features from './components/Features';
+import Contact from './components/Contact';
+
+const App = () => (
+  <div>
+    <Header />
+    <Home />
+    <Features />
+    <Contact />
+  </div>
+);
+
+export default App;
