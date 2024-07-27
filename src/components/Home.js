02371c27051c1f6f40a9d848848a2c57066e4f6c
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2024-07-26 23:14:18 -0500

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+const Home = () => (
+  <section id="home">
+    <h2>Welcome to Cyber Bike</h2>
+    <p>The most futuristic bike with over 100 miles of range.</p>
+  </section>
+);
+
+export default Home;
